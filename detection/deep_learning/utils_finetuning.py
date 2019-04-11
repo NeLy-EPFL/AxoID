b2b9a228b4f6f3750ce3521f9ea30be445ee714c
ntalabot <nicolas.talabot@gmail.com>
2019-04-11 17:59:37 +0200

    Added ROI annotation with OpenCV (paintbrush-like annotations)

--- detection/deep_learning/utils_finetuning.py
+++ detection/deep_learning/utils_finetuning.py
@@ -7,11 +7,12 @@ Created on Mon Apr  8 14:13:08 2019
 @author: nicolas
 """
 
-import time, copy
+import copy
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.widgets as widgets
 from skimage import measure, draw
+import cv2
 
 import torch
 
@@ -116,7 +117,7 @@ def fine_tune(model, inputs, annotations, weights=None, n_iter=200, n_valid=1,
     return model_ft
 
 
-class ROISelector(widgets.LassoSelector):
+class ROIAnnotator_mpl(widgets.LassoSelector):
     """Use matplotlib to draw ROIs for the given images."""
     
     def __init__(self, images):
@@ -207,3 +208,177 @@ class ROISelector(widgets.LassoSelector):
             polygon.remove()
             
             self.update()
+
+
+class ROIAnnotator():
+    """Use OpenCV to draw ROI for the given frames."""
+    
+    def __init__(self, images, window_name="ROI Annotation", rgb2bgr=True):
+        """
+        Start the ROI annotation process with the given images.
+        
+        Args:
+            images: ndarray (N, H, W, C)
+                Numpy array with the input images on which to annotate the ROIs
+            rgb2bgr: bool (default: True)
+                If True, `images` will be converted from RGB to BGR (OpenCV works
+                with BGR).
+        """
+        # Initialization        
+        self.images = images.copy()
+        # Change from RGB to BGR (for OpenCV compatibility)
+        if rgb2bgr:
+            self.images = self.images[..., ::-1]
+        # Make sure images is float32
+        if self.images.dtype == np.uint8:
+            self.images = (self.images / 255).astype(np.float32)
+        elif self.images.dtype == np.float64:
+            self.images = self.images.astype(np.float32)
+        
+        self.window = window_name
+        self.tb_brush_size = "Brush size"
+        self.tb_alpha = "ROI opacity"
+        
+        self.idx = 0 # index of the current image to annotated
+        self.segmentations = np.zeros(self.images.shape[:-1], np.bool)
+        # Lists of arrays with the brushstrokes images (useful for undo/redo)
+        self.brushstrokes = []
+        for _ in range(len(self.images)):
+            self.brushstrokes.append([])
+        self.drawing = False
+        self.x, self.y = -1, -1
+        self.cursor = np.zeros(self.images.shape[1:], self.images.dtype) # cursor image
+        self.border_margin = 5 # number of pixels between overlay and segmentation
+        self.info_height = 50 # height of the bottom information rectangle
+        
+        # Main function (create and destroy window, deal with loop and waitKey, etc.)
+        self.main()
+    
+    def imshow(self):
+        """Draw the current image."""
+        alpha = cv2.getTrackbarPos(self.tb_alpha, self.window) / 10
+        image = self.images[self.idx]
+        # Create the segmentation image
+        self.segmentations[self.idx] = np.max([np.zeros(image.shape[:-1])] + \
+                                               self.brushstrokes[self.idx], axis=0)
+        
+        # Create the overlay of image and drawn ROIs
+        if len(self.brushstrokes[self.idx]) > 0:
+            rois = cv2.cvtColor(self.segmentations[self.idx].astype(image.dtype), 
+                                cv2.COLOR_GRAY2BGR)
+            overlay = (1 - alpha * rois) * image + alpha * rois
+        else:
+            overlay = image.copy()
+        # Create an overlay with the cursor
+        overlay = (1 - alpha * self.cursor) * overlay + alpha * self.cursor
+        
+        # Concatenate with the current segmentation (with a white border)
+        seg = cv2.cvtColor(self.segmentations[self.idx].astype(overlay.dtype), 
+                           cv2.COLOR_GRAY2BGR)
+        img_display = np.concatenate(
+                [overlay, np.ones((image.shape[0], self.border_margin, 3)), seg], axis=1)
+        
+        # Concatenate this image with a rectangle containing informations
+        info_rect = np.ones((self.info_height, img_display.shape[1], 3))
+        _, n_roi = measure.label(self.segmentations[self.idx], connectivity=1, return_num=True)
+        cv2.putText(info_rect, 
+                    "Frame %d/%d" % (self.idx + 1, len(self.images)), 
+                    (image.shape[1] // 2 - 45, self.info_height // 2 - 5),
+                    cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 1, cv2.LINE_AA)
+        cv2.putText(info_rect, 
+                    "%d ROI" % n_roi + ("s" if n_roi > 1 else ""), 
+                    (image.shape[1] // 2 - 25, self.info_height // 2 + 10),
+                    cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 1, cv2.LINE_AA)
+        cv2.putText(info_rect, 
+                    "Resulting segmentation", 
+                    (image.shape[1] + self.border_margin + image.shape[1] // 2 - 95,
+                     self.info_height // 2 + 5),
+                    cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 1, cv2.LINE_AA)
+        final_img = np.concatenate((img_display, info_rect))
+        
+        # Make display image uint8 to have x, y, R, G, B infos in the window
+        final_img = (final_img * 255).astype(np.uint8)
+        
+        cv2.imshow(self.window, final_img)
+    
+    def mouse_callback(self, event, x, y, flags, param):
+        """Mouse callback for the OpenCV window."""
+        # Start drawing if on the image
+        if event == cv2.EVENT_LBUTTONDOWN and \
+           x < self.images.shape[2] and y < self.images.shape[1]:
+            brush_size = cv2.getTrackbarPos(self.tb_brush_size, self.window)
+            self.drawing = True
+            self.x, self.y = x, y
+            self.brushstrokes[self.idx].append(np.zeros(self.images.shape[1:-1]))
+            cv2.line(self.brushstrokes[self.idx][-1], (x, y), (self.x, self.y), 1.0, brush_size)
+            
+        elif event == cv2.EVENT_MOUSEMOVE:
+            brush_size = cv2.getTrackbarPos(self.tb_brush_size, self.window)
+            # Draw the cursor under the mouse
+            self.cursor = np.zeros(self.images.shape[1:], self.images.dtype)
+            cv2.line(self.cursor, (x, y), (x, y), (1.0, 1.0, 1.0), brush_size)
+            
+            # Continue the brush stroke
+            if self.drawing == True:
+                cv2.line(self.brushstrokes[self.idx][-1], (x, y), (self.x, self.y), 1.0, brush_size)
+                self.x, self.y = x, y
+                
+        # Terminate the brush stroke
+        elif event == cv2.EVENT_LBUTTONUP:
+            self.drawing = False
+    
+    def key_callback(self, key):
+        """Key callback for the OpenCV window."""
+        terminate = False
+        
+        # Escape: close the window and stop the annotation
+        if key == 27:
+            print("ESC pressed, annotation interrupted.")
+            terminate = True
+            
+        # Backspace: erase last brushstroke
+        elif key == ord('\b'):
+            if len(self.brushstrokes[self.idx]) > 0:
+                self.brushstrokes[self.idx].pop()
+                
+        # Enter: validate current ROIs and go to the next image
+        elif key == ord('\r') or key == ord('\n'):
+            self.idx += 1
+            if self.idx >= len(self.images):
+                print("Annotation finished.")
+                terminate = True
+        
+        # Navigate images with numbers or right/left arrows
+        elif ord('1') <= key <= ord(str(len(self.images))):
+            self.idx = int(chr(key)) - 1
+        elif key == 81: # left arrow
+            self.idx = max(0, self.idx - 1)
+        elif key == 83: # right arrow
+            self.idx = min(len(self.images) - 1, self.idx + 1)
+                
+        return terminate
+    
+    def main(self):
+        """Main loop of the ROI annotation."""
+        # Create the window
+        cv2.namedWindow(self.window, cv2.WINDOW_NORMAL)
+        cv2.resizeWindow(self.window, 2 * (self.images.shape[2] * 2 + self.border_margin),
+                         2 * self.images.shape[1])
+        cv2.setMouseCallback(self.window, self.mouse_callback)
+        
+        # Create the trackbars
+        cv2.createTrackbar(self.tb_brush_size, self.window, 5, 10, lambda x: None)
+        cv2.setTrackbarMin(self.tb_brush_size, self.window, 1)
+        cv2.createTrackbar(self.tb_alpha, self.window, 5, 10, lambda x: None)
+        
+        # Main loop
+        terminate = False
+        while not terminate:
+            self.imshow()
+            key = cv2.waitKey(33)
+            if key != -1: # a key is pressed
+                key &= 0xFF # keep only last byte
+                terminate = self.key_callback(key)
+        
+        # Terminate the annotation
+        cv2.destroyWindow(self.window)
